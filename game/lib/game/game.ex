50f80cfe31f54c92c21bfb12abf2f88efade6d91
jchery25 <jchery25@yahoo.com>
2019-10-24 17:53:30 -0400

    Working

--- game/lib/game/game.ex
+++ game/lib/game/game.ex
@@ -1,29 +1,39 @@
 defmodule One.Game do
-  def new do
+  def new(starter_player,numOfPlayers) when is_integer(numOfPlayers) do
     deck = initial_deck()
+
     %{
-      players: [],
-      maxPlayers: 2 
+      deck: deck,
+      players: [starter_player],
+      numOfPlayers: numOfPlayers,
+      starter_card: choose_starter_card(deck),
+      winner: nil
     }
   end
 
   def client_view(game) do
     %{
+      deck: game.deck,
       players: game.players,
-      maxPlayers: game.maxPlayers
+      numOfPlayers: game.numOfPlayers,
+      starter_card: game.starter_card,
+      winner: game.winner
     }
   end
 
-  def add_player(game, player_name, max_players) do
-
-    game = Map.put(game, :maxPlayers, max_players)
-
-    fc = game.players
+  def add_player(game, player) do
+    player = game.players
         |> MapSet.new()
-        |> MapSet.put(player_name)
+        |> MapSet.put(player)
         |> MapSet.to_list
 
-    game = Map.put(game, :players, fc)
+      game = Map.put(game, :players, player)
+
+    if(length(game.players) === game.numOfPlayers) do
+      start_game(game)
+    else
+      game
+    end
   end
 
   def add_new_player(game, player_name) do
@@ -33,7 +43,16 @@ defmodule One.Game do
         |> MapSet.to_list
 
     game = Map.put(game, :players, fc)
-    
+    game
+  end
+
+  def start_game(game) do
+    players = for player <- game.players do
+      %{player: "#{player}", cards: Enum.take_random(game.deck, 7)}
+    end
+
+    game = Map.put(game, :players, players)
+    game
   end
 
   def initial_deck do
@@ -45,15 +64,22 @@ defmodule One.Game do
       "\u2660",  "\u2665", "\u2666", "\u2663"
     ]
 
-    value = [
-      1,2,3,4,5,6,7,8,9,10
-    ]
-
     deck = for suit <- suit,
         symbol <- symbol do
           %{suit: "#{suit}", symbol: "#{symbol}"}
     end
     deck = Enum.shuffle(deck)
+    deck
   end
 
+  def choose_starter_card(deck) do
+    card = Enum.take_random(deck, 1)
+    suitCard = get_in(card, [Access.at(0), :suit])
+
+    if (suitCard == "J" || suitCard == "Q" || suitCard == "K" || suitCard == "W") do
+      choose_starter_card(game.deck)
+    end
+
+    card
+  end
 end
